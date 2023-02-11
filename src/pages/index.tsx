a4ccec4fba6aecd09c03882c3836b72800587da6
Archisman Das <72722967+Arch0125@users.noreply.github.com>
2023-02-11 11:35:17 +0530

    initial commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,54 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
+import { ConnectButton } from '@rainbow-me/rainbowkit'
+import { useSendTransaction, usePrepareSendTransaction } from 'wagmi'
+import { onTransaction } from '@/snap/src'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+
+  const defaultSnapOrigin = `local:http://localhost:3000`;
+  const { config } = usePrepareSendTransaction({
+    request: { to: '0xC5Db59D48700B6bC8D53cE773b21931d986DEa0E', value: ('10') },
+  })
+  const { data, isLoading, isSuccess, sendTransaction } =
+    useSendTransaction(config)
+
+   const sendHello = async () => {
+    await window.ethereum?.request({
+      method: 'wallet_invokeSnap',
+      params: [
+        defaultSnapOrigin,
+        {
+          method: 'hello',
+        },
+      ],
+    });
+  };
+
+  const connectSnap = async (
+    snapId: string = defaultSnapOrigin,
+    params: Record<'version' | string, unknown> = {},
+  ) => {
+    await window.ethereum?.request({
+      method:'wallet_enable',
+      params: [
+        {
+          wallet_snap: {
+            [snapId]: {
+              ...params,
+            },
+          },
+        },
+      ],
+
+    });
+  };
+
+  const txdata = onTransaction(config);
+
   return (
     <>
       <Head>
@@ -14,110 +58,14 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+      <div>
+        <ConnectButton/>
+        <button onClick={() => connectSnap()}>Connect Snap</button>
+        <button onClick={() => sendHello()}>Send Hello</button>
+        <button disabled={!sendTransaction} onClick={() => sendTransaction?.()} onClickCapture={()=>sendHello()}>
+        Send Transaction
+      </button>
+      </div>
     </>
   )
 }
